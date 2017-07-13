b261e4405e8e0391f41064dd1dc80a3c2bd49cc3
Ashish Gahlot <dreamashish21@gmail.com>
2017-07-13 23:00:45 +0530

    added i3 config

---
+++ i3/i3install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/supermarin/YosemiteSanFranciscoFont.git && \
+cd YosemiteSnaFranciscoFont/ && \
+cp *.ttf ~/.fonts && \
+sudo apt-get install lxappearance && \
+sudo apt-get install gnome-icon-theme-full && \
+sudo apt-get install rofi && \
+sudo apt-get install compton && \
+sudo apt-get install feh && \
+
